e93897835b6b9bb97e22afc3255c5f5c5a0bfd33
Omar El Hajj <omar.elhajj.00@gmail.com>
2022-09-23 11:53:47 +0300

    add the family font

--- php/get_news.php
+++ php/get_news.php
@@ -7,12 +7,6 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 
-//define the variables
-// $full_name=$_GET["name"];
-// $email=$_GET["email"];
-// $phone=$_GET["phone"];
-// $message=$_GET["message"];
-
 //queries
 $query = $mysqli->prepare("SELECT * FROM news ");
 $query->execute();
